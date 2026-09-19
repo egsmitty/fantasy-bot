0f25bea3268fcdba458b61497179adacff7b5802
egsmitty <ethansmithg@gmail.com>
2026-09-19 13:37:30 -0500

    Check if messages working

--- tele_text.py
+++ tele_text.py
@@ -8,4 +8,6 @@ def send_telegram_message(message):
     bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
     telegram_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    requests.post(telegram_url, data={"chat_id": chat_id, "text": message})
+    response = requests.post(telegram_url, data={"chat_id": chat_id, "text": message})
+    print(response.status_code)
+    print(response.json())
